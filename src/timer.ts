f752c52c1630344381b504cf3f03d877f756cb4f
GaliaBonda <galina.rizhniak@gmail.com>
2021-07-08 00:49:38 +0300

    Correct errors for timer.ts by eslint

--- src/timer.ts
+++ src/timer.ts
@@ -1,21 +1,21 @@
-import * as moment from '../node_modules/moment/moment.js';
+import * as moment from '../node_modules/moment/moment';
 
-let timerInput = <HTMLInputElement>document.getElementById('timer-input');
-let timerMoreBtn = document.getElementById('timer-more-btn');
-let timerLessBtn = document.getElementById('timer-less-btn');
+const timerInput = <HTMLInputElement>document.getElementById('timer-input');
+const timerMoreBtn = document.getElementById('timer-more-btn');
+const timerLessBtn = document.getElementById('timer-less-btn');
 
 timerMoreBtn.onclick = () => {
   if (!timerInput.value) {
     timerInput.value = '0';
   }
-  let inputTime = parseInt(timerInput.value);
-  inputTime++;
+  let inputTime = parseInt(timerInput.value, 10);
+  inputTime += 1;
   timerInput.value = inputTime.toString();
 };
 timerLessBtn.onclick = () => {
-  let inputTime = parseInt(timerInput.value);
+  let inputTime = parseInt(timerInput.value, 10);
   if (inputTime > 0) {
-    inputTime--;
+    inputTime -= 1;
     timerInput.value = inputTime.toString();
   } else {
     alert('Wanna go back in time? Good luck with that!');
@@ -23,64 +23,35 @@ timerLessBtn.onclick = () => {
 };
 
 timerInput.oninput = () => {
-  if (isNaN(parseInt(timerInput.value))) {
+  if (Number.isNaN(parseInt(timerInput.value, 10))) {
     alert('What kind of time is that?');
     timerInput.value = '0';
   }
 };
 
 export function runTimer() {
-  // let timerDiv = document.getElementById('timer');
-  // let timeInput = document.createElement('input');
-  // timerDiv.appendChild(timeInput);
-  // let timeOutput = document.createElement('div');
-  // timerDiv.appendChild(timeOutput);
   let usersTime;
-  let start = document.getElementById('start');
+  const start = document.getElementById('start');
   start.onclick = () => {
     usersTime = timerInput.value;
-    //console.log(timeInput.value);
-    let time = moment({ minute: parseInt(usersTime), second: 0 });
-    //console.log(time);
-    let timerText = document.getElementById('timer-text');
+    const time = moment({ minute: parseInt(usersTime, 10), second: 0 });
+    const timerText = document.getElementById('timer-text');
     timerText.innerHTML = 'Осталось:';
-    let timerMenu = document.getElementById('timer-menu');
+    const timerMenu = document.getElementById('timer-menu');
     timerMenu.style.display = 'none';
     start.style.display = 'none';
-    let timerOutput = document.createElement('div');
+    const timerOutput = document.createElement('div');
     timerOutput.classList.add('timer-output');
-    let timer = document.getElementById('timer');
+    const timer = document.getElementById('timer');
     timer.appendChild(timerOutput);
 
     timerOutput.innerHTML = time.format('mm:ss');
-    let timing = setInterval(function () {
-      let newTime = time.subtract(1, 'seconds');
+    const timing = setInterval(() => {
+      const newTime = time.subtract(1, 'seconds');
       timerOutput.innerHTML = newTime.format('mm:ss');
-      //console.log(newTime.format('mm:ss'));
     }, 1000);
     setTimeout(() => {
       clearInterval(timing);
-    }, parseInt(usersTime) * 1000 * 60);
+    }, parseInt(usersTime, 10) * 1000 * 60);
   };
-
-  // timerInput.onkeydown = (event) => {
-  //     if (event.keyCode === 13) {
-  //         usersTime = timerInput.value;
-  //         //console.log(timeInput.value);
-  //         let time = moment({ minute: parseInt(usersTime), second: 0 });
-  //         //console.log(time);
-  //         let timerText = document.getElementById('timer-text');
-  //         timerText.innerHTML = 'Осталось:';
-  //         let timerMenu = document.getElementById('timer-menu');
-  //         timerMenu.style.display = 'none';
-  //         // timeOutput.innerHTML = time.format('mm:ss');
-  //         // let timing = setInterval(function () {
-  //         //     let newTime = time.subtract(1, 'seconds');
-  //         //     timeOutput.innerHTML = newTime.format('mm:ss');
-  //         //     //console.log(newTime.format('mm:ss'));
-  //         // }, 1000);
-  //         // setTimeout(() => { clearInterval(timing); }, parseInt(usersTime) * 1000 * 60);
-  //     }
-
-  // }
 }
